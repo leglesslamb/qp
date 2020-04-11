17e2f6a59a3add8b62890136980f874d15a7d71d
leglesslamb <wu.rupert@outlook.com>
2020-04-11 15:36:34 -0400

    Replaced dead function --load NAME with --table NAME, deleting the latter.

--- qp.sh
+++ qp.sh
@@ -20,20 +20,19 @@ case $1 in
 Saves and loads filesystem paths as checkpoints, which are saved in $HOME/.config/qp/.
 
 Functions
-    -d, --drop [NAME]         drop: delete checkpoint NAME.
-    -h, --help                help: print this help message.
-    -l, --load [NAME]         load: move to this checkpoint.
-    -s, --save [NAME]         save: save working directory as checkpoint NAME.
-    -s, --save [NAME] [PATH]  save: save (absolute) PATH as checkpoint NAME.
-    -t, --table               table: print table of all checkpoints.
-    -t, --table [NAME]        table: point path of checkpoint NAME.
-    -v, --version             version: print version.
+    -d, --drop NAME       drop: delete checkpoint NAME.
+    -h, --help            help: print this help message.
+    -l, --load NAME       load: print this checkpoint.
+    -s, --save NAME       save: save working directory as checkpoint NAME.
+    -s, --save NAME PATH  save: save (absolute) PATH as checkpoint NAME.
+    -t, --table           table: print table of checkpoints.
+    -v, --version         version: print version.
         "
     ;;
     -l|--load)
         if [[ $# = 2 ]];
         then
-            cd $(cat $HOME/.config/qp/$2)
+            cat $HOME/.config/qp/$2
         fi
     ;;
     -s|--save)
@@ -46,17 +45,12 @@ Functions
         fi
     ;;
     -t|--table)
-        if [[ $# = 2 ]];
-        then
-            cat $HOME/.config/qp/$2
-        else
-            for pt in $(ls $HOME/.config/qp); do
-                echo $pt $(cat $HOME/.config/qp/$pt)
-            done
-        fi
+        for pt in $(ls $HOME/.config/qp); do
+            echo $pt $(cat $HOME/.config/qp/$pt)
+        done
     ;;
     -v|--version)
-        echo "qp (quickpoint) v0.1.0
+        echo "qp (quickpoint) v0.1.1
 Copyright (c) 2020 Robert (Rupert) Wu
 Licence: MIT
         "
